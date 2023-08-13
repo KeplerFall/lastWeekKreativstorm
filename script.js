196cd4896034ca54ee22987e8ebaf57caa6fd64c
omar adam <omar_adam1000@hotmail.com>
2023-08-13 14:01:14 +0300

    fix and merge

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-
-let currentInput = "";
-let expression = "";
-
-function infixToPostfix(infix) {
-    const output = [];
-    const stack = [];
-    
-    const getPrecedence = (op) => {
-        const precedence = {'+': 1, '-': 1, '*': 2, '/': 2};
-        return precedence[op] || 0;
-    };
-=======
 const numButton = document.querySelectorAll('.number');
 const equal = document.querySelector('.equal');
 const operator = document.querySelectorAll('.operator');
@@ -37,112 +23,9 @@ clearAll.addEventListener('click',() => {
     currentNum.textContent = '';
     prevNum.textContent = '';
 });
->>>>>>> omarsBranch
 
     const isOperator = (token) => ['+', '-', '*', '/'].includes(token);
 
-<<<<<<< HEAD
-    for (let token of infix.split(" ")) {
-        if (!isNaN(token) || token.includes(".")) {
-            output.push(token);
-        } else if (isOperator(token)) {
-            while (stack.length && getPrecedence(token) <= getPrecedence(stack[stack.length - 1])) {
-                output.push(stack.pop());
-            }
-            stack.push(token);
-        } else if (token === "(") {
-            stack.push(token);
-        } else if (token === ")") {
-            while (stack.length && stack[stack.length - 1] !== "(") {
-                output.push(stack.pop());
-            }
-            stack.pop();
-        }
-    }
-
-    while (stack.length) {
-        output.push(stack.pop());
-    }
-
-    return output.join(" ");
-}
-
-function evaluatePostfix(postfix) {
-    const stack = [];
-    
-    for (let token of postfix.split(" ")) {
-        if (!isNaN(token) || token.includes(".")) {
-            stack.push(parseFloat(token));
-        } else {
-            const right = stack.pop();
-            const left = stack.pop();
-            switch (token) {
-                case "+":
-                    stack.push(left + right);
-                    break;
-                case "-":
-                    stack.push(left - right);
-                    break;
-                case "*":
-                    stack.push(left * right);
-                    break;
-                case "/":
-                    if (right === 0) return "ERROR";
-                    stack.push(left / right);
-                    break;
-            }
-        }
-    }
-
-    return stack[0].toString();
-}
-
-function buttonInput(value) {
-    const display = document.getElementById("results");
-    const additionalDisplay = document.getElementById("additionalValues");
-    
-    if (["+", "-", "*", "/"].includes(value)) {
-        if (currentInput || expression) {
-            expression += currentInput + " " + value + " ";
-            currentInput = "";
-        }
-    } else if (value === "Ac") {
-        currentInput = "";
-        expression = "";
-    } else if (value === "undo" || value === "Backspace") {
-        currentInput = currentInput.slice(0, -1);
-    } else if (value === "submit" || value === "Enter") {
-        expression += currentInput;
-        const postfix = infixToPostfix(expression);
-        currentInput = evaluatePostfix(postfix);
-        expression = "";
-    } else if (value === "." && currentInput.includes(".")) {
-        return;
-    } else {
-        currentInput += value;
-    }
-    
-    display.value = currentInput;
-    additionalDisplay.textContent = expression;
-}
-
-document.getElementById("results").addEventListener("input", function(event) {
-    currentInput = event.target.value;
-});
-
-document.addEventListener("keydown", function(event) {
-    const key = event.key;
-    if ((key >= "0" && key <= "9") || key === ".") {
-        buttonInput(key);
-    } else if (key === "+" || key === "-" || key === "*" || key === "/") {
-        buttonInput(key);
-    } else if (key === "Enter" || key === "=") {
-        buttonInput("submit");
-    } else if (key === "Backspace") {
-        buttonInput("undo");
-    }
-});
-=======
 undoBtn.addEventListener('click',() => {
     if (currentNum.textContent) {
         currentNum.textContent = currentNum.textContent.slice(0, -1);
@@ -225,4 +108,3 @@ document.addEventListener('keydown', function(event) {
     }
     return number;
 }
->>>>>>> omarsBranch
